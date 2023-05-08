5962080b09cefca8a20d74bd464483bc4c8fad3d
SunkissedQueen <baxtercm@outlook.com>
2023-05-08 10:52:04 -0700

    alert message showing id of selected box

--- src/components/Square.js
+++ src/components/Square.js
@@ -7,7 +7,11 @@ const Square = (props) => {
     <div id="grid">
       {props.board.map((value, index)=> {
         return(
-          <div className="square" key={index}>
+          <div 
+            className="square" 
+            key={index}
+            onClick={() => {props.handleClick(index)}}
+          >
             {value}
           </div>
         )
